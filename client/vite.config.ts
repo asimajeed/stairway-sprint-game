2e8b9cdd2b6d45592910ba42906c13d841a2af68
Asim <asimmajeed202@gmail.com>
2024-11-02 19:07:22 +0500

    Fix deploy.yml

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  base: "stairway-sprint-game",
+  base: "/stairway-sprint-game/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
